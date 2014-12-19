223b08ba8de57ba2b6841d7d521281b9482462cc
ShiftOne Studios <shiftonestudios@Sumeets-iMAC.local>
2014-12-20 05:15:53 +0530

    Code style fixes; Update emblem-compiler dependency

--- index.js
+++ index.js
@@ -4,17 +4,20 @@ var emblem = require('broccoli-emblem-compiler');
 
 module.exports = {
   name: 'ember-cli-emblem',
+
   blueprintsPath: function() {
     return __dirname + '/blueprints';
-  },  
+  },
+
   included: function (app) {
     this._super.included.apply(this, arguments);
     app.registry.add('template', {
       name: 'ember-cli-emblem',
       ext: 'emblem',
+
       toTree: function(tree) {
         return emblem(tree, app.options.emblemOptions);
       }
-    })
+    });
   }
-}
+};
